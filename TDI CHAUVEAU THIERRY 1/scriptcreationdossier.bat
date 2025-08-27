28e34fe658505263d565cb605dd0cb1f2d0f0eb3
TDI CHAUVEAU THIERRY <tdeppc@gmail.com>
2025-08-27 03:29:53 +0200

    Nettoyage du projet - Suppression des anciennes versions

--- TDI CHAUVEAU THIERRY 1/scriptcreationdossier.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-md demonstration1 "demonstration2" "demonstration3" "demonstration4" "demonstration5" "demonstration6" "demonstration7" "demonstration8" "demonstration9" "demonstration10" "demonstration11" "demonstration12" "demonstration13" "demonstration14" "demonstration15"
